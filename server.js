7da7ef4bc95fbffa6aa26dafc59ecb3257f04ea3
ananyananda <me15b163@smail.iitm.ac.in>
2016-09-24 09:03:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/First', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui','First.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -16,9 +18,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/First', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui','First.html'));
-});
+
 
 
 
