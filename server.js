bbb583033e782bc7723e2c0990f48c55aba5a6be
ananyananda <me15b163@smail.iitm.ac.in>
2016-10-31 05:58:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
 app.get('/profile', function(req, res){
     res.sendFile(path.join(__dirname, 'ui','Profile.html'));
 });
+app.get('/profile/add_article', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui','add_article.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
