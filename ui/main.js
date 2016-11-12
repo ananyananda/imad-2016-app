cabcd881e2ed90ca101bc8f3dcd24f9683f5f275
ananyananda <me15b163@smail.iitm.ac.in>
2016-11-12 07:51:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                alert('Logged in successfully');
+            } else if (request.status === 403) {
+                alert('Username/password is incorrect');
+            } else if (request.status === 500) {
+                alert('Something is wrong');
+            }
+        }
+    };
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST', 'http:/ananyananda.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username:username, password:password}));
+};
