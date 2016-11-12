12d2aedaf93aa016a46c589bb997ba36606f6eae
ananyananda <me15b163@smail.iitm.ac.in>
2016-11-12 11:34:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ submit.onclick = function() {
   var password = document.getElementById('password').value;
   console.log(username);
   console.log(password);
-  request.open('POST', 'http:/ananyananda.imad.hasura-app.io/profile', true);
+  request.open('POST', 'http://ananyananda.imad.hasura-app.io/profile', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username:username, password:password}));
 };
